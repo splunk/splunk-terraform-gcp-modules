b6c4dbca5148e0fccd69418314efeca0c4eb8b73
Ankita Kundra <akundra@splunk.com>
2021-10-19 15:59:10 -0700

    Resources name changes

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,7 +1,7 @@
 resource "google_logging_project_sink" "scdm_logging_sink_followers" {
     for_each = toset(var.follower_projects)
     project = each.key
-    name = "${var.scdm_job_uuid}_logging_sink_to_${var.central_project}"
+    name = "logging_sink_to_${var.central_project}_${var.scdm_job_uuid}"
     destination = "pubsub.googleapis.com/${var.pubsub_topic}"
     # TODO: set filter based on log type
     filter = var.log_type == "data_access" ? "logName= (\"projects/${each.key}/logs/cloudaudit.googleapis.com%2Fdata_access\")" : "logName= (\"projects/${each.key}/logs/cloudaudit.googleapis.com%2Factivity\" OR \"projects/${each.key}/logs/cloudaudit.googleapis.com%2Fsystem_event\")"
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "scdm_storage_bucket" {
     project = var.project
-    name = "${var.scdm_job_uuid}_storage_bucket"
+    name = "storage_bucket_${var.scdm_job_uuid}"
 }
 
 resource "google_storage_bucket_object" "scdm_temp_object" {
