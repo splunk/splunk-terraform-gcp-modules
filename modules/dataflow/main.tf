bc6c52275fc1155b58a403e8a1335d148c291fe8
alexl <alexl@splunk.com>
2022-01-12 16:14:18 -0800

    specify machine type n2-standard-2 for dataflow job

--- modules/dataflow/main.tf
+++ modules/dataflow/main.tf
@@ -5,6 +5,7 @@ resource "google_dataflow_job" "scdm_dataflow_job" {
     template_gcs_path = "gs://dataflow-templates/${var.pubsub_to_splunk_template_version}/Cloud_PubSub_to_Splunk"
     temp_gcs_location = "gs://${var.temp_gcs_location}/tmp"
     max_workers = var.max_workers
+    machine_type = "n2-standard-2"
     parameters = {
         # https://cloud.google.com/dataflow/docs/guides/templates/provided-streaming#pubsub-to-splunk
         inputSubscription = var.pubsub_subscription
