2e52c922c3668e22e4f465064b70c993141a8e63
ikaur-splunk <89876791+ikaur-splunk@users.noreply.github.com>
2022-05-26 16:22:06 -0700

    Delete older logging module

--- modules/logging/main.tf
+++
@@ -1,8 +0,0 @@
-resource "google_logging_project_sink" "scdm_data_logging_sinks" {
-    for_each = toset(var.data_projects)
-    project = each.key
-    name = var.logging_name
-    destination = "pubsub.googleapis.com/${var.pubsub_topic}"
-    filter = replace(var.filter,"PROJECT_ID_DM_TMP",each.key)
-    unique_writer_identity = true
-}
--- modules/logging/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "unique_writer_identity_members" {
-  value = values(google_logging_project_sink.scdm_data_logging_sinks)[*].writer_identity
-}
