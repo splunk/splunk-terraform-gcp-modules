b784891efbcba483d571a3eb96ff4c0dda5c3545
“ikaur-splunk” <ikaur@splunk.com>
2022-06-01 15:52:09 -0700

    support logs ingestion from GCP folders

---
+++ modules/folder_iam_binding/main.tf
@@ -0,0 +1,22 @@
+resource "google_project_iam_custom_role" "scdm_readonly_role" {
+  for_each    = toset(var.projects)
+  project     = each.key
+  role_id     = var.role_id
+  title       = var.role_title
+  description = var.role_description
+  permissions = var.roles_list
+}
+
+locals {
+  role_ids = {for k, v in google_project_iam_custom_role.scdm_readonly_role : "${k}" => v.id }
+}
+
+
+resource "google_project_iam_binding" "scdm_service_account_binding_with_readonly_role" {
+  for_each = toset(var.projects)
+  project  = each.key
+  role     = lookup(local.role_ids, "${each.key}")
+  members = [
+    "serviceAccount:${var.service_account_email}",
+  ]
+}
