8b030259e43ae868ab85fca6d00c1f8ff8b0d50e
ikaur-splunk <89876791+ikaur-splunk@users.noreply.github.com>
2022-07-28 15:59:20 -0700

    fix folder iam bindings (#13)

--- modules/folder_iam_binding/main.tf
+++ modules/folder_iam_binding/main.tf
@@ -1,4 +1,5 @@
 resource "google_organization_iam_custom_role" "scdm_readonly_role" {
+  count       = (var.create_module == true) ? 1 : 0
   org_id      = var.parent_organization
   role_id     = var.role_id
   title       = var.role_title
@@ -7,14 +8,14 @@ resource "google_organization_iam_custom_role" "scdm_readonly_role" {
 }
 
 locals {
-  role_id = google_organization_iam_custom_role.scdm_readonly_role.role_id
+  role_id = google_organization_iam_custom_role.scdm_readonly_role[*].role_id
 }
 
 resource "google_folder_iam_binding" "scdm_service_account_binding_with_readonly_role" {
   for_each = toset(var.folders)
-  folder = "folders/${each.key}"
-  role   = "organizations/${var.parent_organization}/roles/${local.role_id}"
-  members = [
+  folder   = "folders/${each.key}"
+  role     = "organizations/${var.parent_organization}/roles/${local.role_id[0]}"
+  members  = [
     "serviceAccount:${var.service_account_email}",
   ]
 }
